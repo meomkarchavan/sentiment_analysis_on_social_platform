f549ab5c43f3da4a8bc0cbd6d2660f35751329f4
meomkarchavan <chavanomkar245@gmail.com>
2021-05-15 17:12:30 +0530

    lineterminator added

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -18,13 +18,13 @@ def read_and_concat(path,df,date_column):
 
 def read_and_clean_csv(path,date_column=None):
   if date_column:
-    df=pd.read_csv(path,parse_dates=[date_column], date_parser=dateparse )
+    df=pd.read_csv(path,parse_dates=[date_column], date_parser=dateparse ,lineterminator='\n')
     try:
       df=df.drop('Unnamed: 0',axis=1)
     except KeyError:
       pass
   else:
-    df=pd.read_csv(path,index_col=False )
+    df=pd.read_csv(path,index_col=False,lineterminator='\n')
     try:
       df=df.drop('Unnamed: 0',axis=1)
     except KeyError:
